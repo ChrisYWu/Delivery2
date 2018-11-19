b9230751853a10cd6e701c27a7929c12dfaf93e1
Chris <Chris.Wu@live.com>
2018-11-19 12:22:30 -0600

    Some updates to give me all access

--- Delivery2/A0006. Merch-GrandUserAccess.sql
+++ Delivery2/A0006. Merch-GrandUserAccess.sql
@@ -1,14 +1,21 @@
 use Merch
 Go
 
-Select *
-From Setup.UserLocation
-Where GSN = 'GUPPX008'
-Go
+Delete From Setup.UserLocation
+Where UserLocationID in (
+	Select Min(UserLocationID) UserLocationID 
+	From Setup.UserLocation
+	Where GSN = 'WUXYX001'
+	Group By SAPBranchID
+	Having Count(*) = 2
+)
+
 
 Insert Setup.UserLocation(GSN, SAPBranchID, LastModified, LastModifiedBy)
-Select Distinct 'GUPPX008' GSN, SAPBranchID, GetDAte(), 'WUXYX001' LastModifiedBy
+Select Distinct 'WUXYX001' GSN, SAPBranchID, GetDAte(), 'WUXYX001' LastModifiedBy
 From Setup.UserLocation
-Where GSN != 'GUPPX008'
+Where SAPBranchID Not In (Select Distinct SAPBranchID From Setup.UserLocation Where GSN = 'WUXYX001')
 Order By SAPBranchID
 Go
+
+
--- Delivery2/C0005. Merch System Statistics.sql
+++ Delivery2/C0005. Merch System Statistics.sql
@@ -6,7 +6,6 @@ Go
 
 /*
 1. Collecting 35K images every day, total of 11 milion images
-
 */
 Select IsOrphaned, Count(*) Cnt
 From Operation.AzureBlobStorage
@@ -46,7 +45,7 @@ Group By OperationDate, Datepart(Hour, UTCInsertTime)
 3. Registered user over life time: 7.8K
 Active merchandisers: 3.6K
 800~1000 dispatches every day
-4000 Route Updtes every day
+4000 Route Updates every day
 
 */
 Select Count(*)
@@ -71,3 +70,23 @@ From (
 ) tmp
 Group By DispatchDate
 Order By DispatchDate
+
+
+/*
+Fidn today's built
+*/
+
+Select GroupName, inn.SAPAccountNumber, AccountName, a.State
+From Operation.MerchStopCheckIn inn 
+Join SEtup.MerchGroup m on inn.MerchGroupID = m.MerchGroupID
+Join SAP.Account a  on a.SAPAccountNumber = inn.SAPAccountNumber
+	Where inn.SAPAccountNumber in (
+	Select Top 100 SAPAccountNumber
+	From Operation.DisplayBuild db
+	Join Operation.DisplayBuildExecution dbe on db.LatestExecutionID = dbe.DisplayBuildExecutionID
+	Where dbe.BuildStatusID = 2
+	Order By dbe.LastModified desc
+	)
+And Inn.DispatchDate = Convert(DAte, GEtDAte())
+Order By GroupName
+
