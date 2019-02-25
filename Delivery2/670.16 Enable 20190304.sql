986d436c5ba775b320dbd1238f36f3eebb33f538
Wu <chris.wu@kdrp.com>
2019-02-25 14:56:51 -0600

    jim again

---
+++ Delivery2/670.16 Enable 20190304.sql
@@ -0,0 +1,49 @@
+Use Portal_Data
+Go
+
+Select @@SERVERNAME Server, DB_Name() As [Database]
+Go
+
+Alter Proc dbo.pEnableBranchesForMesh
+As
+	Insert Shared.Feature_Authorization(FeatureID, BranchID, IsActive)
+	Select 6 FeatureID, SAPBranchID BranchID, 1 IsActive
+	From SAP.Branch
+	Where SAPBranchID in (
+	1009,1064,1065
+	)
+	And SAPBranchID <> 'TJW1'
+	And SAPBranchID Not In (Select BranchID From Shared.Feature_Authorization Where FeatureID = 6)
+
+	Declare @V Varchar(8000)
+	Set @V = '' 
+
+	Select @V = @V + Convert(Varchar(10), BranchID) + ','
+	From Shared.Feature_Authorization
+	Where FeatureID = 6
+	Order By BranchID
+
+	Select SUBSTRING(@V, 1, Len(@V) - 1) ConfigValue
+	-- 1103,1104,1108,1109,1113,1114,1115,1116,1117,1120,1138,1178,1184 Before changes
+	-- 1103,1104,1108,1109,1113,1114,1115,1116,1117,1120,1138,1178,1184 SetupValue
+
+	Update Merch.Setup.Config
+	Set Value = SUBSTRING(@V, 1, Len(@V) - 1)
+	Where ConfigID = 4
+Go
+
+exec dbo.pEnableBranchesForMesh
+Go
+
+Select Count(*) Cnt 
+From Shared.Feature_Authorization
+Where FeatureID = 6
+
+Select *
+From Merch.Setup.Config
+Go
+
+Select *
+From Portal_Data.Shared.Feature_Authorization
+Where featureid = 6
+Go
